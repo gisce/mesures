0e94ecacdc05106391a204a40cd3740877238a35
puig <francescpuig7@gmail.com>
2021-02-16 16:37:58 +0100

    Add P2 and P2D support

--- mesures/p2.py
+++ mesures/p2.py
@@ -1,9 +1,20 @@
 from mesures.p1 import P1
+from mesures.headers import P1_HEADER as columns
 
 class P2(P1):
     def __init__(self, data, distributor=None):
         super(P2, self).__init__(data, distributor)
         self.prefix = 'P2'
 
+    def reader(self):
+        for key in ['ai', 'ae', 'r1', 'r2', 'r3', 'r4']:
+            if key not in df:
+                df[key] = 0
+            df[key] = df[key].astype('int32')
+
     def writer(self):
-        pass
+        """
+        P2 contains a curve files diary on zip
+        :return: file path
+        """
+        return super(P2, self).writer()
--- mesures/p2d.py
+++ mesures/p2d.py
@@ -1,9 +1,26 @@
 from mesures.p2 import P2
+from mesures.headers import P1_HEADER as columns
 
 class P2D(P2):
     def __init__(self, data, distributor=None):
         super(P2D, self).__init__(data, distributor)
         self.prefix = 'P2D'
+        self.default_compression = 'bz2'
+
+    @property
+    def filename(self):
+        return "{prefix}_{distributor}_{timestamp}.{version}".format(
+            prefix=self.prefix, distributor=self.distributor, timestamp=self.generation_date.strftime('%Y%m%d'), version=self.version
+        )
 
     def writer(self):
-        pass
+        """
+        P2D contains all curve measure in one file
+        :return: file path
+        """
+        file_path = os.path.join('/tmp', self.filename) + '.' + self.default_compression
+        self.file.to_csv(
+            file_path, sep=';', header=False, columns=columns, index=False, line_terminator=';\n',
+            compression=self.default_compression
+        )
+        return file_path
