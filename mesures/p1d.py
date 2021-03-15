f74264b30b2a1ba583aca7a9e808950e38d425c4
puig <francescpuig7@gmail.com>
2021-03-15 17:01:03 +0100

    Add comer on PD files

--- mesures/p1d.py
+++ mesures/p1d.py
@@ -3,10 +3,11 @@ from mesures.p1 import P1
 from mesures.headers import P1_HEADER as columns
 
 class P1D(P1):
-    def __init__(self, data, distributor=None):
+    def __init__(self, data, distributor=None, comer=None):
         super(P1D, self).__init__(data, distributor)
         self.prefix = 'P1D'
         self.default_compression = 'bz2'
+        self.comer = comer
 
     @property
     def filename(self):
--- mesures/p2d.py
+++ mesures/p2d.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import os
 from mesures.p2 import P2
-from mesures.headers import P1_HEADER as columns
+from mesures.headers import P2_HEADER as columns
 
 class P2D(P2):
-    def __init__(self, data, distributor=None):
+    def __init__(self, data, distributor=None, comer=comer):
         super(P2D, self).__init__(data, distributor)
         self.prefix = 'P2D'
         self.default_compression = 'bz2'
+        self.comer = comer
 
     @property
     def filename(self):
