eef923207f81317310d4a88c66cf7f3b766b1e92
puig <francescpuig7@gmail.com>
2021-04-19 16:27:18 +0200

    Add F1 test

--- spec/generation_files/files_spec.py
+++ spec/generation_files/files_spec.py
@@ -7,7 +7,7 @@ from spec.sample_data import get_sample_data
 
 
 with description('An F1') as self:
-    with it('instance of F1 Class'):
+    with it('is instance of F1 Class'):
         data = get_sample_data()
         f = F1(data)
         assert isinstance(f, F1)
@@ -18,6 +18,16 @@ with description('An F1') as self:
         res = f.writer()
         import zipfile
         assert zipfile.is_zipfile(res)
+
+    with it('has its class methods'):
+        data = get_sample_data()
+        f = F1(data)
+        res = f.writer()
+        import numpy as np
+        assert isinstance(f.total, (int, np.int64))
+        assert f.ai == f.total
+        assert isinstance(f.cups, list)
+        assert isinstance(f.number_of_cups, int)
 
 
 with description('An P1') as self:
