8611143af7f46dc20adf17acee4393a64b8e96a4
Markus <mruiz@gisce.net>
2021-12-28 14:51:20 +0100

    Add changes on ALMACENACAU

--- mesures/almacenacau.py
+++ mesures/almacenacau.py
@@ -7,13 +7,13 @@ from mesures.dates.date import REE_END_DATE
 from mesures.parsers.dummy_data import DummyKeys
 
 class ALMACENACAU(object):
-    def __init__(self, data, distributor=None):
+    def __init__(self, data, distributor=None, compression='bz2'):
         data = DummyKeys(data).data
         self.file = self.reader(data)
         self.generation_date = datetime.now()
         self.prefix = 'ALMACENACAU'
         self.version = 0
-        self.default_compression = 'bz2'
+        self.default_compression = compression
         self.distributor = distributor
 
     def __repr__(self):
@@ -30,10 +30,17 @@ class ALMACENACAU(object):
 
     @property
     def filename(self):
-        return "{prefix}_{distributor}_{timestamp}.{version}".format(
-            prefix=self.prefix, distributor=self.distributor,
-            timestamp=self.generation_date.strftime('%Y%m%d'), version=self.version
-    )
+        if self.default_compression:
+            return "{prefix}_{distributor}_{timestamp}.{version}.{compression}".format(
+                prefix=self.prefix, distributor=self.distributor,
+                timestamp=self.generation_date.strftime('%Y%m%d'),
+                version=self.version, compression=self.default_compression
+            )
+        else:
+            return "{prefix}_{distributor}_{timestamp}.{version}".format(
+                prefix=self.prefix, distributor=self.distributor,
+                timestamp=self.generation_date.strftime('%Y%m%d'), version=self.version
+            )
 
     def reader(self, file_path):
         if isinstance(file_path, str):
@@ -60,7 +67,7 @@ class ALMACENACAU(object):
         """
         :return: file path of generated ALMACENACAU File
         """
-        file_path = os.path.join('/tmp', self.filename) + '.' + self.default_compression
+        file_path = os.path.join('/tmp', self.filename)
         self.file.to_csv(
             file_path, sep=';', header=False, columns=columns, index=False, line_terminator=';\n',
             compression=self.default_compression
