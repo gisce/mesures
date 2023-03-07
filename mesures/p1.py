577dc59844878d162f7a570059ebf24daa68089f
davidmunoznovoa <dmunoz@gisce.net>
2023-03-07 15:48:05 +0100

    Temp commit

--- mesures/p1.py
+++ mesures/p1.py
@@ -136,19 +136,26 @@ class P1(object):
             df = (datetime.strptime(daymin, DATETIME_MASK) + timedelta(days=1)).strftime(DATETIME_MASK)
             self.measures_date = di
             dataf = self.file[(self.file['timestamp'] >= di) & (self.file['timestamp'] < df)]
-            # dataf['timestamp'] = dataf['timestamp'].apply(lambda x: x.strftime(DATETIME_HOUR_MASK))
-            file_path = os.path.join('/tmp', self.filename)
-            kwargs = {'sep': ';',
-                      'header': False,
-                      'columns': self.columns,
-                      'index': False,
-                      'line_terminator': ';\n'
-                      }
-            if self.default_compression:
-                kwargs.update({'compression': self.default_compression})
+            if len(dataf):
+                existing_files = os.listdir('/tmp')
+                if existing_files:
+                    versions = [int(f.split('.')[1]) for f in existing_files if self.zip_filename.split('.')[0] in f]
+                    if versions:
+                        self.version = max(versions) + 1
+                # dataf['timestamp'] = dataf['timestamp'].apply(lambda x: x.strftime(DATETIME_HOUR_MASK))
+                file_path = os.path.join('/tmp', self.filename)
+                kwargs = {'sep': ';',
+                          'header': False,
+                          'columns': self.columns,
+                          'index': False,
+                          'line_terminator': ';\n'
+                          }
+                if self.default_compression:
+                    kwargs.update({'compression': self.default_compression})
 
-            dataf.to_csv(file_path, **kwargs)
+                dataf.to_csv(file_path, **kwargs)
+                zipped_file.write(file_path, arcname=os.path.basename(file_path))
+
             daymin = df
-            zipped_file.write(file_path, arcname=os.path.basename(file_path))
         zipped_file.close()
         return zipped_file.filename
--- mesures/p1d.py
+++ mesures/p1d.py
@@ -41,6 +41,12 @@ class P1D(P1):
         """
         :return: file path of generated P1D File
         """
+        existing_files = os.listdir('/tmp')
+        if existing_files:
+            versions = [int(f.split('.')[1]) for f in existing_files if self.filename.split('.')[0] in f]
+            if versions:
+                self.version = max(versions) + 1
+
         file_path = os.path.join('/tmp', self.filename)
         kwargs = {'sep': ';',
                   'header': False,
