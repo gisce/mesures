c8f1f6fa6992d30a1e45cb663090586c2efad3d5
davidmunoznovoa <dmunoz@gisce.net>
2023-04-18 13:34:54 +0200

    Revert "Cleaner solution"
    This reverts commit 54449a62e07dfb0c7bd2c4869b60e3714566718b.

--- mesures/utils.py
+++ mesures/utils.py
@@ -2,11 +2,10 @@ import inspect
 import pandas as pd
 
 def check_line_terminator_param():
-    # 'line_terminator' is deprecated in pandas
-    # 'getargspec' is deprecated in python3
-    if not hasattr(inspect, 'getargspec'):
-        inspect.getargspec = inspect.getfullargspec
-    params = inspect.getargspec(pd.DataFrame.to_csv).args
+    try:
+        params = inspect.getargspec(pd.DataFrame.to_csv).args
+    except ValueError:
+        params = inspect.getfullargspec(pd.DataFrame.to_csv).args
     if 'line_terminator' in params:
         return 'line_terminator'
     else:
