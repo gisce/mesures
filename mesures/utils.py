b5e33b9f516347d94a429805ef88c8a02319fdc2
davidmunoznovoa <dmunoz@gisce.net>
2023-04-18 13:28:15 +0200

    Fix inspection in Python3

--- mesures/utils.py
+++ mesures/utils.py
@@ -2,7 +2,11 @@ import inspect
 import pandas as pd
 
 def check_line_terminator_param():
-    if 'line_terminator' in inspect.getargspec(pd.DataFrame.to_csv).args:
+    try:
+        params = inspect.getargspec(pd.DataFrame.to_csv).args
+    except ValueError:
+        params = inspect.getfullargspec(pd.DataFrame.to_csv).args
+    if 'line_terminator' in params:
         return 'line_terminator'
     else:
         return 'lineterminator'
