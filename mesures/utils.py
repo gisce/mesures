54449a62e07dfb0c7bd2c4869b60e3714566718b
davidmunoznovoa <dmunoz@gisce.net>
2023-04-18 13:33:05 +0200

    Cleaner solution

--- mesures/utils.py
+++ mesures/utils.py
@@ -2,10 +2,11 @@ import inspect
 import pandas as pd
 
 def check_line_terminator_param():
-    try:
-        params = inspect.getargspec(pd.DataFrame.to_csv).args
-    except ValueError:
-        params = inspect.getfullargspec(pd.DataFrame.to_csv).args
+    # 'line_terminator' is deprecated in pandas
+    # 'getargspec' is deprecated in python3
+    if not hasattr(inspect, 'getargspec'):
+        inspect.getargspec = inspect.getfullargspec
+    params = inspect.getargspec(pd.DataFrame.to_csv).args
     if 'line_terminator' in params:
         return 'line_terminator'
     else:
