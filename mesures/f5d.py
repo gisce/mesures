b9aceb369eb20e5e3ac47ebcfc4718adc24b5564
davidmunoznovoa <dmunoz@gisce.net>
2025-09-02 13:20:36 +0200

    Improve version control to avoid collisions #2

--- mesures/f5d.py
+++ mesures/f5d.py
@@ -113,11 +113,13 @@ class F5D(F5):
         :return: file path of generated F5D File
         """
         existing_files = os.listdir('/tmp')
-        if self.default_compression != 'zip':
-            if existing_files:
+        if existing_files:
+            if self.default_compression != 'zip':
                 versions = [int(f.split('.')[1]) for f in existing_files if self.filename.split('.')[0] in f and '.zip' not in f]
-                if versions:
-                    self.version = max(versions) + 1
+            else:
+                versions = [int(f.split('.')[1]) for f in existing_files if self.filename.split('.')[0] in f and '.zip' in f]
+            if versions:
+                self.version = max(versions) + 1
 
         file_path = os.path.join('/tmp', self.filename)
         kwargs = {'sep': ';',
